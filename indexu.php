2b4edef296cff8a16f770a3f63fcc9b2f8570194
Andoria Coss <59074955+Andoria-Coss@users.noreply.github.com>
2022-05-25 00:30:32 -0500

    Add files via upload

---
+++ indexu.php
@@ -0,0 +1,26 @@
+<?php
+/*************************************************************/
+/* Archivo:  index.php
+ * Objetivo: página inicial de manejo de catálogo,
+ *           incluye manejo de sesiones y plantillas
+ * Autor:
+ *************************************************************/
+include_once("cabecera.html");
+include_once("menu.php");
+include_once("aside.html");
+//
+?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h1 align="center"> Bienvenido usuario</h1>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php
+include_once("pie.html");
+?>
